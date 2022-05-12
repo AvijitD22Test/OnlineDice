bc4bb5f42543472497354c0404a3f4a9e688ab66
Avijit Das <73435734+AvijitD22Test@users.noreply.github.com>
2022-05-12 21:14:03 +0530

    Update diceJs.js

--- diceJs.js
+++ diceJs.js
@@ -1,22 +1,22 @@
-function diceRoll(position){
-    var formula = Math.round(Math.random() * 100);
-    if (formula < 16){
-        document.querySelectorAll("img")[position].setAttribute("src" , "./images/dice1.png");
+function diceRoll(position) {
+    var formula = Math.round(Math.random() * 6 + 1);
+    if (formula == 1) {
+        document.querySelectorAll("img")[position].setAttribute("src", "./images/dice1.png");
         return 1;
-    }else if (formula > 16 && Math.round(Math.random() * 100) < 32){
-        document.querySelectorAll("img")[position].setAttribute("src" , "./images/dice2.png");
+    } else if (formula == 2) {
+        document.querySelectorAll("img")[position].setAttribute("src", "./images/dice2.png");
         return 2
-    }else if (formula > 32 && Math.round(Math.random() * 100) < 48){
-        document.querySelectorAll("img")[position].setAttribute("src" , "./images/dice3.png");
+    } else if (formula == 3) {
+        document.querySelectorAll("img")[position].setAttribute("src", "./images/dice3.png");
         return 3
-    }else if (formula > 48 && Math.round(Math.random() * 100) < 64){
-        document.querySelectorAll("img")[position].setAttribute("src" , "./images/dice4.png");
+    } else if (formula == 4) {
+        document.querySelectorAll("img")[position].setAttribute("src", "./images/dice4.png");
         return 4
-    }else if (formula > 64 && Math.round(Math.random() * 100) < 80){
-        document.querySelectorAll("img")[position].setAttribute("src" , "./images/dice5.png");
+    } else if (formula == 5) {
+        document.querySelectorAll("img")[position].setAttribute("src", "./images/dice5.png");
         return 5
-    }else {
-        document.querySelectorAll("img")[position].setAttribute("src" , "./images/dice6.png");
+    } else {
+        document.querySelectorAll("img")[position].setAttribute("src", "./images/dice6.png");
         return 6
     }
 }
@@ -24,10 +24,10 @@ function diceRoll(position){
 var first = diceRoll(0);
 var second = diceRoll(1);
 
-if (first > second){
-    document.querySelector("h1").innerHTML = "<i>🚩</i>Player 1 Won";
-}else if(second > first){
-    document.querySelector("h1").innerHTML = "<i>🚩</i>Player 2 Won";
-}else{
-    document.querySelector("h1").innerHTML = "DRAW";
+if (first > second) {
+    document.querySelector("h1").innerHTML = "🚩Player 1 Won!";
+} else if (second > first) {
+    document.querySelector("h1").innerHTML = "Player 2 Won🚩!";
+} else {
+    document.querySelector("h1").innerHTML = "Draw!";
 }
