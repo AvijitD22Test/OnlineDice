4c4208941aaf208eba8806a10049d139788d1374
Avijit Das <73435734+AvijitD22Test@users.noreply.github.com>
2022-05-13 09:15:30 +0530

    Update diceJs.js

--- diceJs.js
+++ diceJs.js
@@ -1,32 +1,16 @@
-function diceRoll(position) {
-    var formula = Math.round(Math.random() * 6);
-    if (formula == 1) {
-        document.querySelectorAll("img")[position].setAttribute("src", "./images/dice1.png");
-        return 1;
-    } else if (formula == 2) {
-        document.querySelectorAll("img")[position].setAttribute("src", "./images/dice2.png");
-        return 2
-    } else if (formula == 3) {
-        document.querySelectorAll("img")[position].setAttribute("src", "./images/dice3.png");
-        return 3
-    } else if (formula == 4) {
-        document.querySelectorAll("img")[position].setAttribute("src", "./images/dice4.png");
-        return 4
-    } else if (formula == 5) {
-        document.querySelectorAll("img")[position].setAttribute("src", "./images/dice5.png");
-        return 5
-    } else {
-        document.querySelectorAll("img")[position].setAttribute("src", "./images/dice6.png");
-        return 6
-    }
+function randomImage(position){
+    formula = Math.ceil(Math.random() * 6);
+    var myImage = "images/dice" + formula + ".png";
+    document.querySelectorAll("img")[position].setAttribute("src", myImage)
+    return formula;
 }
 
-var first = diceRoll(0);
-var second = diceRoll(1);
+var firstImage  = randomImage(0);
+var secondImage = randomImage(1);
 
-if (first > second) {
+if (firstImage > secondImage) {
     document.querySelector("h1").innerHTML = "🚩Player 1 Won!";
-} else if (second > first) {
+} else if (secondImage > firstImage) {
     document.querySelector("h1").innerHTML = "Player 2 Won🚩!";
 } else {
     document.querySelector("h1").innerHTML = "Draw!";
